71302ad15a90b628daa23f0d15e52124b68dea60
pgm-michielwillems <michiel.willems@student.arteveldehs.be>
2021-12-17 22:06:04 +0100

    finished btn-totop

--- static/js/btn-totop.js
+++ static/js/btn-totop.js
@@ -3,10 +3,11 @@ $btn = document.getElementById('btn-totop');
 $main = document.querySelector('.container-main');
 $users = document.querySelector('.box-github--users');
 $main.addEventListener('scroll', scrollDown);
+$users.addEventListener('scroll', scrollDown);
 $btn.addEventListener('click', toTop);
 
 function scrollDown() {
-  if ($main.scrollTop > 100) {
+  if ($main.scrollTop > 100 || $users.scrollTop > 100) {
     $btn.style.opacity = '1';
   } else {
     $btn.style.opacity = '0';
